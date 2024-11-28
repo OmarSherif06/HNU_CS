b8d9e44d2139abee6377561f84ca4c408b9b0b64
OmarSherif06 <omar.sherif.nabawy@gmail.com>
2024-11-28 19:56:43 +0200

    Even or Odd (using a conditional/ternary operator)

---
+++ C/Even-or-Odd-(using-a-ternary-operator)/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    (num % 2 == 0) ? printf("%d is an even number", num) : printf("%d is an odd number", num);
+
+    return 0;
+}
