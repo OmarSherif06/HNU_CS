096031974f3f9bb443edc1334a8b87b2f5f4d0fc
OmarSherif06 <omar.sherif.nabawy@gmail.com>
2024-12-16 22:14:41 +0200

    Write a C program that defines a function 'printMenu' to print a menu for a calculator program. The function should not accept any arguments.

---
+++ C/printMenu-function/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void printMenu() {
+    printf("Calculator Menu:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n0. Exit");
+}
+
+int main() {
+
+    printMenu();
+
+    return 0;
+}
