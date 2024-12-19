26a89cf4a80d2842b2004c53b51da24a6ba85cb4
OmarSherif06 <omar.sherif.nabawy@gmail.com>
2024-12-19 09:42:50 +0200

    Added 'recursiveFactorial'

--- C/factorial-function/main.c
+++ C/factorial-function/main.c
@@ -10,13 +10,19 @@ int factorial(int number) {
     return product;
 }
 
+int recursiveFactorial(int number) {
+    if (number <= 0) return 1;
+
+    return number * recursiveFactorial(number - 1);
+}
+
 int main() {
     int number;
 
     printf("Enter number: ");
     scanf("%d", &number);
 
-    printf("Factorial of %d: %d", number, factorial(number));
+    printf("Factorial of %d: %d", number, recursiveFactorial(number));
 
     return 0;
 }
