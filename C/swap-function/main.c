4697311b3b818b554690421ec2884151503ef4eb
OmarSherif06 <omar.sherif.nabawy@gmail.com>
2024-12-19 09:26:13 +0200

    Write a C program that defines a function 'swap' to swap the values of two variables. The function should accept two integer arguments and swap their values.

---
+++ C/swap-function/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("a = %d, b = %d\n", a, b);
+
+    return 0;
+}
